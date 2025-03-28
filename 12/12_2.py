3825dd7f7f91a19aec1ffe7b4db9e0e9f4a29d57
olshom <olga.shomarova@protonmail.com>
2025-03-28 11:53:39 +0200

    12_2

--- 12/12_2.py
+++ 12/12_2.py
@@ -1,12 +1,15 @@
 import requests
 
-#https://api.chucknorris.io/jokes/random
-pyyntö = ""
+#
+city = input('What is your city? ')
+API_KEY = 'bbb62d7f22db6325a33a57ed2f3ab5e6'
+pyyntö = f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&APPID={API_KEY}"
 try:
     vastaus = requests.get(pyyntö)
     if vastaus.status_code == 200:
         json_vastaus = vastaus.json()
-        print(json_vastaus['value'])
+        print(json_vastaus["weather"][0]["main"],
+              json_vastaus["main"]["temp"], '°C')
 
 except requests.exceptions.RequestException as e:
     print('something went wrong')
